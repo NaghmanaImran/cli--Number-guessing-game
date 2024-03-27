d96bdd07f02f5d76b96905c05035ada6611b1780
Naghmanaimran <imran2244ansari@gmail.com>
2024-03-28 00:05:41 +0500

    Adding a guessing game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("welcome to my code - cli guessing game number");
+const randomNumber = Math.floor(Math.random()*5+1);
+const answer = await inquirer.prompt([{
+    name :"usrGuessNumber",
+    type: "number",
+    message : "Enter your guess Number(Number limit from 1 to 5):",
+
+}]);
+if(answer.usrGuessNumber === randomNumber){
+    console.log("congratulation!,you guess a correct number");
+}else(
+    console.log("sorry, you guess wrong number")
+);
